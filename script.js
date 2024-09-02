5c8aa6c31d054302d0c7bbd6b114f596c4718c2e
gopiraju9000 <144523885+gopiraju9000@users.noreply.github.com>
2024-09-02 11:48:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const extendHex = (shortHex) => {
   // write your code here
-	return shortHex[0]+shortHex[1]+shortHex[1]+shortHex[2]+shortHex[2]+shortHex[3]+shortHex[3];
+	return shortHex[0]+shortHex[0]+shortHex[1]+shortHex[1]+shortHex[2]+shortHex[2];
 };
 
 // Do not change the code below.
